6503d18bc1a246af2981c63b8abd20508d4bbb56
Xiang Wang <1846468966@qq.com>
2016-03-28 09:20:56 +0800

    抓取了第一批高铁数据。

--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Xiang Wang @ 2016-03-27 16:41:11
 
-import datetime, requests, time, json, logging
+import datetime, requests, time, json, logging, os
 
 from requests.exceptions import Timeout
 
@@ -10,7 +10,7 @@ URL = 'http://train.qunar.com/qunar/checiInfo.jsp'
 DEFAULT_DATE = datetime.datetime.now() + datetime.timedelta(30,0,0)
 DEFAULT_DATE_STR = DEFAULT_DATE.strftime('%Y-%m-%d') # 默认查询30天后的时刻表
 
-logging.basicConfig(level=logging.DEBUG,
+logging.basicConfig(level=logging.WARN,
                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                 datefmt='%a, %d %b %Y %H:%M:%S',
                 filename='./log/crawler.log',
@@ -54,16 +54,19 @@ def getinfo(train_number = 'G99', date=DEFAULT_DATE_STR):
         logging.error(params)
         return None
     result = json.loads(response.text)
-    if result== {'count':'0'}:
+    if result== {'count':0}:
         trainfile = open('./data/不存在的火车车次','a')
         trainfile.write(train_number+'\n')
         trainfile.close()
         return None
     return response.text
 
-def main():
-    for i in range(1,9730):
-        time.sleep(5)
+def main(start=1):
+    for i in range(start,9730):
+        if 0 <= datetime.datetime.now().hour <= 6:
+            time.sleep(2)   # 凌晨服务器压力小2秒一次，一晚上就能爬完
+        else:
+            time.sleep(10)   # 10秒一次，不要太快
         train = 'G' + str(i)
         result = getinfo(train_number = train)
         if result:
@@ -73,4 +76,11 @@ def main():
             gotfile.close()
         
 if __name__ == '__main__':
-    main()
+    if len(os.sys.argv) > 1:
+        start = int(os.sys.argv[1])
+        print('输入了参数 %d, 爬虫将会从该班次开始抓取'%(start))
+        main(start)
+    else:
+        print('爬虫脚本已启动, 使用"tail -f ./data/火车班次json数据" 可以实时查看抓取的数据')
+        main()
+    print('抓取完毕，请进入data文件夹查看数据， 进入log文件夹查看日志')
